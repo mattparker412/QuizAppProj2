7d61e11558937122dbea09751d339b11366bd9bb
Matthew <mattparker412@gmail.com>
2022-03-14 13:29:41 -0400

    Added facebook functionality

--- QuizAppProj2/ViewController.swift
+++ QuizAppProj2/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let loginButton = FBLoginButton()
+        loginButton.permissions = ["public_profile", "email"]
+        loginButton.center = view.center
+        view.addSubview(loginButton)
+        if let token = AccessToken.current,
+                !token.isExpired {
+                // User is logged in, do work such as go to next view controller.
+           
+            }
+        else{
+            
+        }
     }
 
 
