67d2624bf42a2a96ca5f6133cf39ba480cecbb21
Matthew <mattparker412@gmail.com>
2022-03-18 16:55:06 -0400

    final fix files

---
+++ QuizAppProj2/ViewLogicComponents/QuizCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionDemo
+//
+//  Created by Asha Rani on 25/02/22.
+//
+
+import UIKit
+
+class QuizCollectionViewCell: UICollectionViewCell {
+    
+   
+    @IBOutlet weak var mlb1: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        contentView.layer.cornerRadius = 5.0
+        contentView.layer.masksToBounds = true
+        layer.cornerRadius = 5.0
+        layer.masksToBounds = false
+       // layer.frame.
+        
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor.black.cgColor
+        contentView.layer.backgroundColor = UIColor.white.cgColor
+        backgroundColor = UIColor.blue
+}
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+
+    
+    
+
+    
+    
+}
