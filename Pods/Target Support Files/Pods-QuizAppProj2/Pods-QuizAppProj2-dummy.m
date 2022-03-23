9e76bd83d297a8a7a3646aaed47636dae16456b9
Matthew <mattparker412@gmail.com>
2022-03-23 16:10:23 -0400

    rid of all pod files

--- Pods/Target Support Files/Pods-QuizAppProj2/Pods-QuizAppProj2-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QuizAppProj2 : NSObject
-@end
-@implementation PodsDummy_Pods_QuizAppProj2
-@end
--- Pods/Target Support Files/Pods-QuizAppProj2Tests/Pods-QuizAppProj2Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QuizAppProj2Tests : NSObject
-@end
-@implementation PodsDummy_Pods_QuizAppProj2Tests
-@end
