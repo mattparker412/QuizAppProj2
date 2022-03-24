a3535428a940805373dffe82ad5f465ba78fc7aa
john726 <revature726@revature.net>
2022-03-24 14:55:00 -0700

    ui updates

---
+++ QuizAppProj2/ViewControllers/LaunchscreenAnimationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LaunscreenAnimationViewController.swift
+//  QuizAppProj2
+//
+//  Created by John Figueroa on 3/24/22.
+//
+
+import UIKit
+
+class LaunchscreenAnimationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+}
