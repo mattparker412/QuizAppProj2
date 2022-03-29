b356f39a5fa7832412adebea6486272e6e04fb85
john726 <revature726@revature.net>
2022-03-29 13:33:58 -0700

    even more comments

--- QuizAppProj2/ViewControllers/ChatViewController.swift
+++ QuizAppProj2/ViewControllers/ChatViewController.swift
@@ -48,26 +48,17 @@ class ChatViewController: MessagesViewController, MessagesDataSource, MessagesLa
     func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
         return messages.count
     }
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(userName!)
-        print("message to append")
-        print(messageToAppend)
         messagesCollectionView.messagesDataSource = self
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
         messageInputBar.delegate = self
-        
         for index in 0..<db.getFeedBacks().count{
             var name = db.getFeedBacks()[index]["name"]
             var message = db.getFeedBacks()[index]["feedback"]
-            print(name)
-            print(message)
             name = db.getUserName(userId: Int(name!)!)
             message = message! + " - " + name!
-            print(message!)
             messages.append(Message(sender: otherUser, messageId: String(messages.count+index+1), sentDate: Date(), kind: .text(message!)))
         }
     }
